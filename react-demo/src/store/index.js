796b4ba6de820c539bf8be334047015ec2e2e6f3
shixinlei <shi_xinlei@qq.com>
2019-08-25 19:00:50 +0800

    0.0.1

--- react-demo/src/store/index.js
+++ react-demo/src/store/index.js
@@ -2,16 +2,10 @@ import { createStore , applyMiddleware, compose } from "redux"
 import reducer from "./reducer"
 import createSagaMiddleware from "redux-saga"
 import mySaga from "./mySaga"
-
-
 const sagaMiddleware = createSagaMiddleware();
 const composeEnhance = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? 
 window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}):compose;
-
 const enhance = composeEnhance(applyMiddleware(sagaMiddleware));
-
 const store = createStore(reducer, enhance);
-
-sagaMiddleware.run(mySaga)
-
-export default store
+sagaMiddleware.run(mySaga);
+export default store;
